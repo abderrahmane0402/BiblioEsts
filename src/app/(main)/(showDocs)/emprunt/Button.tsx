2af0926d7fd3101a8cc57ef54bdec708951b204e
abderrahmane0402 <abderrahmanesabkari@gmail.com>
2023-05-10 23:15:34 +0100

    show docs emprunt 70%

---
+++ src/app/(main)/(showDocs)/emprunt/Button.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export const Button = ({ name }: { name: string }) => {
+  const path = usePathname();
+  const link = path.split("/");
+  const [active, setactive] = useState(false);
+  useEffect(() => {
+    setactive(`${name}`.includes(path));
+    console.log(path.includes(name));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    setactive(path.includes(name));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [path]);
+  return (
+    <button
+      className={`${
+        active ? "bg-black/80 text-white" : ""
+      } w-24 text-lg py-2 rounded-md bg-transparent hover:bg-black hover:text-white duration-500 transition-colors`}
+    >
+      <Link href={`/emprunt/${name}/${link[3]}/${link[4]}`}>{name}</Link>
+    </button>
+  );
+};
+
+export const Button2 = ({ name }: { name: string }) => {
+  const path = usePathname();
+  const link = path.split("/");
+  const [active, setactive] = useState(false);
+  useEffect(() => {
+    setactive(`${name}`.includes(path));
+    console.log(path.includes(name));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    setactive(path.includes(name));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [path]);
+  return (
+    <button
+      className={`${
+        active ? "bg-lime-600/80 text-white" : ""
+      } w-24 text-lg py-2 rounded-md bg-transparent hover:bg-lime-600 hover:text-white duration-500 transition-colors`}
+    >
+      <Link href={`/emprunt/${link[2]}/${name}/${link[4]}`}>{name}</Link>
+    </button>
+  );
+};
+
+export const Button3 = ({ name }: { name: string }) => {
+  const path = usePathname();
+  const link = path.split("/");
+  const [active, setactive] = useState(false);
+  useEffect(() => {
+    setactive(`${name}`.includes(path));
+    console.log(path.includes(name));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    setactive(path.includes(name));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [path]);
+  return (
+    <button
+      className={`${
+        active ? "bg-red-700/80 text-white" : ""
+      } w-24 text-lg py-2 rounded-md bg-transparent hover:bg-red-700 hover:text-white duration-500 transition-colors`}
+    >
+      <Link href={`/emprunt/${link[2]}/${link[3]}/${name}`}>{name}</Link>
+    </button>
+  );
+};
